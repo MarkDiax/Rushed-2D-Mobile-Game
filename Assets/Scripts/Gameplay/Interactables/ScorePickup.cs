5231b2d9c7e1552010e5a19589dae764a5115655
Mark Offenberg <mark-offenberg@hotmail.com>
2018-09-18 21:51:25 +0200

    Added custom brushes, pickups and more
    - Custom brushes gives the ability to spawn and delete gameobjects and instantiate prefabs in tilemaps.
    - Added scorepickups, spawnable with the PrefabBrush.
    - Improved the folder structure in some folders.
    - Redone some of the rendering/sorting order.

---
+++ Assets/Scripts/Gameplay/Interactables/ScorePickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Tilemaps;
+
+public class ScorePickup : MonoBehaviour
+{
+	public int score;
+	public UnityEvent OnScorePickup;
+
+	public void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject == Player.Instance.gameObject) {
+			Player.Instance.playerData.AddScore(score);
+			OnScorePickup.Invoke();
+
+			Destroy(gameObject);
+		}
+	}
+}
